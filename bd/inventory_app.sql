fe84d2d1df400238a94190d1c3dcdc94c0cce642
KEVINOLIVEROS2024 <oliveroskevin2024@gmail.com>
2024-08-11 19:37:51 -0500

    v6

--- bd/inventory_app.sql
+++ bd/inventory_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-07-2024 a las 23:04:18
+-- Tiempo de generación: 12-08-2024 a las 02:36:58
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,6 +24,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `activos`
+--
+
+CREATE TABLE `activos` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tipo_de_activo` varchar(255) NOT NULL,
+  `numero_activo` varchar(255) NOT NULL,
+  `serial_activo` varchar(255) NOT NULL,
+  `marca` varchar(255) NOT NULL,
+  `modelo` varchar(255) NOT NULL,
+  `fecha_lanzamiento` date NOT NULL,
+  `fecha_compra` date NOT NULL,
+  `valor` decimal(15,2) NOT NULL,
+  `estado` varchar(255) NOT NULL,
+  `sedes_id` bigint(20) UNSIGNED NOT NULL,
+  `ubicaciones_id` bigint(20) UNSIGNED NOT NULL,
+  `sububicaciones_id` bigint(20) UNSIGNED NOT NULL,
+  `users_id` bigint(20) UNSIGNED NOT NULL,
+  `categorias_id` bigint(20) UNSIGNED NOT NULL,
+  `proveedores_id` bigint(20) UNSIGNED NOT NULL,
+  `garantia` date DEFAULT NULL,
+  `notas_activos` text DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `activos`
+--
+
+INSERT INTO `activos` (`id`, `tipo_de_activo`, `numero_activo`, `serial_activo`, `marca`, `modelo`, `fecha_lanzamiento`, `fecha_compra`, `valor`, `estado`, `sedes_id`, `ubicaciones_id`, `sububicaciones_id`, `users_id`, `categorias_id`, `proveedores_id`, `garantia`, `notas_activos`, `created_at`, `updated_at`) VALUES
+(2, 'Laptop', '1212122', '1212122', 'Lenovo', '2021', '2020-12-12', '2021-08-25', 5000000.00, 'Activo', 1, 7, 11, 1, 1, 1, '2023-12-12', 'Mantenimiento', '2024-08-11 07:45:49', '2024-08-11 07:46:47');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `cache`
 --
 
@@ -38,8 +74,8 @@ CREATE TABLE `cache` (
 --
 
 INSERT INTO `cache` (`key`, `value`, `expiration`) VALUES
-('a17961fa74e9275d529f489537f179c05d50c2f3', 'i:1;', 1722282740),
-('a17961fa74e9275d529f489537f179c05d50c2f3:timer', 'i:1722282740;', 1722282740);
+('a17961fa74e9275d529f489537f179c05d50c2f3', 'i:1;', 1723372440),
+('a17961fa74e9275d529f489537f179c05d50c2f3:timer', 'i:1723372440;', 1723372440);
 
 -- --------------------------------------------------------
 
@@ -52,6 +88,26 @@ CREATE TABLE `cache_locks` (
   `owner` varchar(255) NOT NULL,
   `expiration` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `categoria` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `categoria`, `created_at`, `updated_at`) VALUES
+(1, 'laptop', '2024-08-11 05:38:09', '2024-08-11 05:38:09');
 
 -- --------------------------------------------------------
 
@@ -124,7 +180,13 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '0001_01_01_000000_create_users_table', 1),
 (2, '0001_01_01_000001_create_cache_table', 1),
 (3, '0001_01_01_000002_create_jobs_table', 1),
-(4, '2024_07_29_194721_create_products_table', 2);
+(4, '2024_07_29_194721_create_products_table', 1),
+(5, '2024_08_05_203429_create_sedes_table', 1),
+(6, '2024_08_05_203452_create_ubicaciones_table', 1),
+(7, '2024_08_05_203515_create_sububicaciones_table', 1),
+(8, '2024_08_05_203555_create_categorias_table', 1),
+(9, '2024_08_05_203610_create_proveedores_table', 1),
+(10, '2024_08_08_181054_create_activos_table', 1);
 
 -- --------------------------------------------------------
 
@@ -153,15 +215,52 @@ CREATE TABLE `products` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
--- Volcado de datos para la tabla `products`
+-- Estructura de tabla para la tabla `proveedores`
 --
 
-INSERT INTO `products` (`id`, `name`, `quantity`, `price`, `created_at`, `updated_at`) VALUES
-(3, 'laptop', 3, 10.00, '2024-07-30 01:23:03', '2024-07-30 01:23:03'),
-(4, 'impresora', 4, 10.00, '2024-07-30 01:27:36', '2024-07-30 01:27:36'),
-(5, 'mouse', 5, 10.00, '2024-07-30 01:27:46', '2024-07-30 01:27:46'),
-(6, 'celulares', 6, 80.00, '2024-07-30 01:27:59', '2024-07-30 01:27:59');
+CREATE TABLE `proveedores` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `proveedor` varchar(255) NOT NULL,
+  `direccion` varchar(255) NOT NULL,
+  `telefono` varchar(255) NOT NULL,
+  `correo` varchar(255) NOT NULL,
+  `paginaweb` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `proveedores`
+--
+
+INSERT INTO `proveedores` (`id`, `proveedor`, `direccion`, `telefono`, `correo`, `paginaweb`, `created_at`, `updated_at`) VALUES
+(1, 'Lenovo', 'United Estates', '1', 'soporte@lenovo.com', 'www.lenovo.com', '2024-08-11 05:39:07', '2024-08-11 05:39:07');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sedes`
+--
+
+CREATE TABLE `sedes` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `ciudad` varchar(255) NOT NULL,
+  `sede` varchar(255) NOT NULL,
+  `direccion` varchar(255) NOT NULL,
+  `telefono` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `sedes`
+--
+
+INSERT INTO `sedes` (`id`, `ciudad`, `sede`, `direccion`, `telefono`, `created_at`, `updated_at`) VALUES
+(1, 'Bogota', 'Norte', '209', '1', '2024-08-11 05:37:31', '2024-08-11 05:37:31');
 
 -- --------------------------------------------------------
 
@@ -183,9 +282,62 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('3MvLAgFLEuN6LxTGVsn9kxLt455lsAzB6Y5reGtm', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36', 'YTo3OntzOjY6Il90b2tlbiI7czo0MDoiQ2Vxd3ZicDlFRzc4ck9JVDFGMmE1eDZ4cjZBM0hjNWg2eDVacHp2TiI7czozOiJ1cmwiO2E6MDp7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6OToiX3ByZXZpb3VzIjthOjE6e3M6MzoidXJsIjtzOjM2OiJodHRwOi8vbG9jYWxob3N0OjgwMDAvYWRtaW4vcHJvZHVjdHMiO31zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO3M6MTc6InBhc3N3b3JkX2hhc2hfd2ViIjtzOjYwOiIkMnkkMTIkajlBbmZieHJISzFMcjFmN09RNndldTFuSUpRSXRoZkNOc2tTME1RaTRpcTlSUUpPZFJvVFMiO3M6ODoiZmlsYW1lbnQiO2E6MDp7fX0=', 1722284881),
-('4Jz59wSNeWJl9mT7KznefKuvwsmsGLyW4npVAix6', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36', 'YToyOntzOjY6Il90b2tlbiI7czo0MDoieGFHMGtrTmZtQnZsODlJYUxaU29MZGUwMXJQeVRFeHNLUmYzNzJraCI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1722281154),
-('Bof2th5HLJVaIh9G7JssS7RikGgDy8ygy7znwpXJ', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiWVYzUTNGUlVrWm0wTTJZYThBWWx5Q1BSS3dIMWJRejcwNUhWekk4TyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1722281005);
+('8RgBm9WOFlSEgjKzjApMw2DHRHIUKNxdrg0ZuP7V', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiUjZrSVQyTmc0aFI4cUxiemVWWVZLR2piVWJMeXhSQ1V4ZEFKVVBvcSI7czozOiJ1cmwiO2E6MTp7czo4OiJpbnRlbmRlZCI7czo0MjoiaHR0cDovLzEyNy4wLjAuMTo4MDAwL2FkbWluL2FjdGl2b3MvY3JlYXRlIjt9czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NDI6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbi9hY3Rpdm9zL2NyZWF0ZSI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1723372333),
+('9YLvaOqOyvkqkRb8ubA5xm532Kkt08kaNdOo6HtA', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36', 'YTo3OntzOjY6Il90b2tlbiI7czo0MDoiUjduNEtEcXdoN3JJVml0Z2pJOEF2RjhmTjNqQ09BYkg4cU01N21FSCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mzk6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbi91YmljYWNpb25lcyI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6MzoidXJsIjthOjA6e31zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO3M6MTc6InBhc3N3b3JkX2hhc2hfd2ViIjtzOjYwOiIkMnkkMTIkYXVUcWR6eXU3ZW9hcWRDQWIwdWxoLjlkUGxOL2YxcXRWN1BrNDJZem5ud3FpUHFHSUNrWEMiO3M6ODoiZmlsYW1lbnQiO2E6MDp7fX0=', 1723344528),
+('HevLQcHRS9Gt0PMKIzxXlsZK4DeIqOzDOqkscJxS', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36', 'YTo2OntzOjY6Il90b2tlbiI7czo0MDoibjJjUnZ3bU5vMUR6MnVrSG1sejJWdmUySUIzQkRGWjZ3MlhzY2M2ZyI7czozOiJ1cmwiO2E6MDp7fXM6OToiX3ByZXZpb3VzIjthOjE6e3M6MzoidXJsIjtzOjM5OiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYWRtaW4vdWJpY2FjaW9uZXMiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO3M6MTc6InBhc3N3b3JkX2hhc2hfd2ViIjtzOjYwOiIkMnkkMTIkYXVUcWR6eXU3ZW9hcWRDQWIwdWxoLjlkUGxOL2YxcXRWN1BrNDJZem5ud3FpUHFHSUNrWEMiO30=', 1723372848);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sububicaciones`
+--
+
+CREATE TABLE `sububicaciones` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `sububicacion` varchar(255) NOT NULL,
+  `ubicacion_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `sububicaciones`
+--
+
+INSERT INTO `sububicaciones` (`id`, `sububicacion`, `ubicacion_id`, `created_at`, `updated_at`) VALUES
+(6, 'Rectoria', 4, '2024-08-10 21:15:43', '2024-08-10 21:15:43'),
+(7, 'Logistica', 4, '2024-08-11 07:35:29', '2024-08-11 07:35:29'),
+(8, '110', 6, '2024-08-11 07:37:48', '2024-08-11 07:37:48'),
+(9, 'Sistemas', 7, '2024-08-11 07:40:47', '2024-08-11 07:40:47'),
+(10, 'Laboratorio', 7, '2024-08-11 07:41:01', '2024-08-11 07:41:01'),
+(11, 'Oficina Soporte', 7, '2024-08-11 07:41:48', '2024-08-11 07:41:48'),
+(12, 'Comunicaciones', 4, '2024-08-11 07:42:54', '2024-08-11 07:42:54'),
+(13, 'Admisiones', 4, '2024-08-11 07:43:07', '2024-08-11 07:43:07'),
+(14, 'Vicerrectoria', 4, '2024-08-11 07:43:33', '2024-08-11 07:43:33');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ubicaciones`
+--
+
+CREATE TABLE `ubicaciones` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `ubicacion` varchar(255) NOT NULL,
+  `tipo` enum('edificio','bloque') NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `ubicaciones`
+--
+
+INSERT INTO `ubicaciones` (`id`, `ubicacion`, `tipo`, `created_at`, `updated_at`) VALUES
+(4, 'Edificio Administrativo', 'edificio', '2024-08-10 21:12:42', '2024-08-10 21:12:42'),
+(6, 'Bloque A', 'bloque', '2024-08-10 21:14:00', '2024-08-10 21:14:00'),
+(7, 'Bloque B', 'bloque', '2024-08-11 07:11:12', '2024-08-11 07:39:14'),
+(8, 'Biblioteca', 'edificio', '2024-08-11 07:40:04', '2024-08-11 07:40:04');
 
 -- --------------------------------------------------------
 
@@ -209,12 +361,23 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'kevin', 'kevin@uni.com.co', NULL, '$2y$12$j9AnfbxrHK1Lr1f7OQ6weu1nIJQIthfCNskS0MQi4iq9RQJOdRoTS', NULL, '2024-07-30 00:22:14', '2024-07-30 00:22:14'),
-(2, 'Pedro Sarmiento', 'pedro@inventory.com', NULL, '$2y$12$SiVZVRj9kgNBdkyXLqOmbuo2Ir.zEJso9qXHa.mYxoDvrmWIxgTKi', NULL, '2024-07-30 01:21:17', '2024-07-30 01:21:17');
+(1, 'kevin', 'kevin@emp.com', NULL, '$2y$12$auTqdzyu7eoaqdCAb0ulh.9dPlN/f1qtV7Pk42YznnwqiPqGICkXC', NULL, '2024-08-10 20:46:29', '2024-08-10 20:46:29');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `activos`
+--
+ALTER TABLE `activos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `activos_sedes_id_foreign` (`sedes_id`),
+  ADD KEY `activos_ubicaciones_id_foreign` (`ubicaciones_id`),
+  ADD KEY `activos_sububicaciones_id_foreign` (`sububicaciones_id`),
+  ADD KEY `activos_users_id_foreign` (`users_id`),
+  ADD KEY `activos_categorias_id_foreign` (`categorias_id`),
+  ADD KEY `activos_proveedores_id_foreign` (`proveedores_id`);
 
 --
 -- Indices de la tabla `cache`
@@ -227,6 +390,12 @@ ALTER TABLE `cache`
 --
 ALTER TABLE `cache_locks`
   ADD PRIMARY KEY (`key`);
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `failed_jobs`
@@ -267,12 +436,37 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `sedes`
+--
+ALTER TABLE `sedes`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `sessions`
 --
 ALTER TABLE `sessions`
   ADD PRIMARY KEY (`id`),
   ADD KEY `sessions_user_id_index` (`user_id`),
   ADD KEY `sessions_last_activity_index` (`last_activity`);
+
+--
+-- Indices de la tabla `sububicaciones`
+--
+ALTER TABLE `sububicaciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sububicaciones_ubicacion_id_foreign` (`ubicacion_id`);
+
+--
+-- Indices de la tabla `ubicaciones`
+--
+ALTER TABLE `ubicaciones`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `users`
@@ -284,6 +478,18 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `activos`
+--
+ALTER TABLE `activos`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `failed_jobs`
@@ -301,19 +507,64 @@ ALTER TABLE `jobs`
 -- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `sedes`
+--
+ALTER TABLE `sedes`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `sububicaciones`
+--
+ALTER TABLE `sububicaciones`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de la tabla `ubicaciones`
+--
+ALTER TABLE `ubicaciones`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `activos`
+--
+ALTER TABLE `activos`
+  ADD CONSTRAINT `activos_categorias_id_foreign` FOREIGN KEY (`categorias_id`) REFERENCES `categorias` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `activos_proveedores_id_foreign` FOREIGN KEY (`proveedores_id`) REFERENCES `proveedores` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `activos_sedes_id_foreign` FOREIGN KEY (`sedes_id`) REFERENCES `sedes` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `activos_sububicaciones_id_foreign` FOREIGN KEY (`sububicaciones_id`) REFERENCES `sububicaciones` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `activos_ubicaciones_id_foreign` FOREIGN KEY (`ubicaciones_id`) REFERENCES `ubicaciones` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `activos_users_id_foreign` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `sububicaciones`
+--
+ALTER TABLE `sububicaciones`
+  ADD CONSTRAINT `sububicaciones_ubicacion_id_foreign` FOREIGN KEY (`ubicacion_id`) REFERENCES `ubicaciones` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
