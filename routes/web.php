0cbe912f111a4bf230a0b7562335734c89064d2b
KEVINOLIVEROS2024 <oliveroskevin2024@gmail.com>
2024-10-01 16:34:47 -0500

    v1

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,10 @@ use App\Http\Middleware\CheckRoleMiddleware;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/users/search', [UserSearchController::class, 'search']);
 Route::middleware(['auth', 'check.role:super_admin'])->group(function () {
     Route::get('/users/search', [UserSearchController::class, 'search']);
     // Otras rutas protegidas
 });
+
